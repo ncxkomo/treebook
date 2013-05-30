379e069281236d8d03f2fd2785e651f71d892f3c
Ry Ma <ncxkomo@gmail.com>
2013-05-30 15:46:05 +0200

    Added tests for user fname, lname, profile correctness

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,15 @@ class User < ActiveRecord::Base
   					:first_name, :last_name, :profile_name
   # attr_accessible :title, :body
 	
+  validates :first_name, presence: true 
+  validates :last_name, presence: true 
+  validates :profile_name, presence: true,
+                          uniqueness: true, # see if another user exists with same profile name in our db when we save this user
+                          format: {
+                            with: /a-zA-Z0-9_-/, #regular expression that makes sure it can include lower, upper case, 0-9, _ and -
+                            message: "Must be formatted correctly." # must spit back same error as in test
+                          }
+
   has_many :statuses
 
   def full_name
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,41 @@
 require 'test_helper'
 
-class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+class UserTest < ActiveSupport::TestCase # IN THE TESTS, WE TRY TO SAVE INCORRECT DATA TO THE DATABASE
+
+	test "a user should enter a first name" do 
+		user = User.new # creates a new user variable, user is a new instance of the User class 
+		assert !user.save # tells use the user should not be saved in our database, assert asserts that something is true
+		assert !user.errors[:first_name].empty? # asserts that the errors on the first_name field are not empty
+	end
+
+	test "a user should enter a last name" do 
+		user = User.new
+		assert !user.save # Q: asserts that the user was not saved?
+		assert !user.errors[:last_name].empty? # Q: asserts that the user does not have an empty lname?
+	end
+
+	test "a user should enter a profile name" do 
+		user = User.new
+		assert !user.save
+		assert !user.errors[:profile_name].empty?
+	end
+
+	test "a user should have a unique profile name" do 
+		user = User.new
+		user.profile_name = users(:rydawg).profile_name # created a fixture
+		assert !user.save
+		# puts user.errors.inspect # spits out errors along the way
+		assert !user.errors[:profile_name].empty?
+	end
+
+	test "a user should have a profile name without spaces" do 
+		user = User.new
+		user.profile_name = "My Profile"
+		assert !user.save # make sure that it can't be saved
+		assert !user.errors[:profile_name].empty? # make sure that there are some errors in the profile name
+		# we're doing this to make sure errors are registered. when we initially run the test without entering validations in the user profile, we see that errors were NOT registered. by entering validations, we confirm errors DO GET registered.
+		# when i first ran this, the assertion failed because no errors were in the profile name. i don't want this, i want errors to be registered. that's why we then put in the validations.
+		assert user.errors[:profile_name].include?("Must be formatted correctly.") # checks errors array with the profilename key to make sure w'ere gettting the right error
+	end
+
 end
